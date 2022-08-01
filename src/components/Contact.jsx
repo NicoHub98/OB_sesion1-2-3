75f26a1d695f8b7fb76e6216579236c67e539f2e
NicoHub98 <nicolud98@gmail.com>
2022-08-01 17:49:03 -0300

    v1

---
+++ src/components/Contact.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ContactClass } from "../model/ContactClass";
+
+function Contact({ contacto }) {
+  return (
+    <div>
+      <h3>Nombre: {contacto.nombre} </h3>
+      <h3>Apellido: {contacto.apellido} </h3>
+      <h3>Email: {contacto.email} </h3>
+      <h3>
+        {contacto.conectado ? "Contacto en línea" : "Contacto no disponible"}
+      </h3>
+    </div>
+  );
+}
+
+Contact.propTypes = {
+  contacto: PropTypes.instanceOf(ContactClass),
+  nombre: PropTypes.string,
+  apellido: PropTypes.string,
+  email: PropTypes.string,
+  conectado: PropTypes.bool,
+};
+
+export default Contact;
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ContactClass } from "../model/ContactClass";
+import Contact from "./Contact";
+
+function ContactList() {
+  const defaultContact = new ContactClass(
+    "Nombre default",
+    "Apellido default",
+    "email@mail.com",
+    true
+  );
+
+  return (
+    <div>
+      <h1>Contacto:</h1>
+      <Contact contact={defaultContact} />
+    </div>
+  );
+}
+
+export default ContactList;
